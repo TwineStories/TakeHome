bc584aae6e0d8e596fadb58fd9e7ee6e9fd495f6
Ankit Singh Rana <ankitsdev88@gmail.com>
2023-06-21 16:12:32 +0530

    UI Design for navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/home';
+import { Art } from './components/art';
+import { Collab } from './components/collab';
+import { Create } from './components/create';
 
 function App() {
   return (
     <div className="App">
-      <h1>Twine Take Home Assessment</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/art" element={<Art />} />
+        <Route path="/collab" element={<Collab />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <h1>Twine Take Home Assessment</h1>
+    </div>
+  )
+}
